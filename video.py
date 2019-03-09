5ece1a27ef5b633e771706ebcda2f47c80ee58fb
NedBeans0 <tomosdavies115@gmail.com>
2019-03-09 01:12:26 +0000

    broke

--- video.py
+++ video.py
@@ -8,9 +8,10 @@ mnist = tf.keras.datasets.mnist #28x28 handwritten digits
 
 #unpacking to training and testing variables
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_train, axis=1)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()   # 28x28 numbers of 0-9
+x_train = tf.keras.utils.normalize(x_train, axis=1).reshape(x_train.shape[0], -1)
+x_test = tf.keras.utils.normalize(x_test, axis=1).reshape(x_test.shape[0], -1)
+
 
 #scaling/normalising
 #tensor values are now scaled between 0 and 1
@@ -19,16 +20,19 @@ x_test = tf.keras.utils.normalize(x_train, axis=1)
 model = tf.keras.models.Sequential()
 #first layer is input layer
 #our images are 28*28 in a multidimensional array so let's make them flat
-model.add(tf.keras.layers.Flatten())
+
+
+#model.add(tf.keras.layers.Flatten())
 
 #set density of neural network
 #128 NODES
 #activation function is what tells the
 #neurons to 'fire' - we are using the
 #sort of go-to ones
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu, input_shape= x_train.shape[1:]))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.softmax))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.softmax))   #10 because dataset is numbers from 0 - 9
+
 #we have 3 layers
 
 #Now for parameters for the model's training
@@ -56,3 +60,5 @@ predictions = new_model.predict([x_test])
  
 print(np.argmax(predictions[0]))
 
+plt.imshow(x_test[0])
+plt.show() 
